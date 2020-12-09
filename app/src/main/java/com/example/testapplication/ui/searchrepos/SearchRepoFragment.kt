d7397f5e88f4e4f346cda60a3b27f4347cada23c
amiratashani <amiratashanii@gmail.com>
2020-12-09 09:46:34 +0330

    config timber

--- app/src/main/java/com/example/testapplication/ui/searchrepos/SearchRepoFragment.kt
+++ app/src/main/java/com/example/testapplication/ui/searchrepos/SearchRepoFragment.kt
@@ -5,11 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.testapplication.R
+import com.example.testapplication.adapter.RepoAdapter
+import com.example.testapplication.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_search_repo.*
+import timber.log.Timber
+
 
 @AndroidEntryPoint
 class SearchRepoFragment : Fragment() {
+
+    private val viewModel: SearchRepoViewModel by viewModels()
+
+    private lateinit var adapter: RepoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,12 +35,37 @@ class SearchRepoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecycleView()
+        setupObservers()
+        viewModel.search("Android")
+    }
 
+    private fun setupRecycleView() {
+        adapter = RepoAdapter()
+        list.adapter = adapter
+    }
+
+    private fun setupObservers() {
+        viewModel.repos.observe(viewLifecycleOwner) { result ->
+            when (result.status) {
+                Resource.Status.SUCCESS -> {
+                    Timber.e("SUCCESS")
+                    Timber.e("${result.data}")
+                    progress_bar.visibility = View.GONE
+                    if (!result.data.isNullOrEmpty()) adapter.submitList(result.data)
+                }
+                Resource.Status.ERROR -> {
+                }
+                Resource.Status.LOADING -> {
+                    Timber.e("LOADING")
+                    progress_bar.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchRepoFragment().apply {}
+        fun newInstance() = SearchRepoFragment().apply {}
     }
 }
